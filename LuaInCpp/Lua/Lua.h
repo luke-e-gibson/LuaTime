df7e20602b250f489e385190d6f8243624086a33
luke-e-gibson <luke.e.gibson@outlook.com>
2024-07-09 16:27:06 +1000

    Move Lua class to namespace for added LuaLib enum

--- LuaInCpp/Lua/Lua.h
+++ LuaInCpp/Lua/Lua.h
@@ -1,19 +1,35 @@
 #include <sol/sol.hpp>
+#include <spdlog/spdlog.h>
 #include "assert.h"
 
-
-class LuaVm 
+namespace Lua
 {
-private:
-	sol::state lua;
-	int runtime_exception_handler(lua_State* L, sol::optional<const std::exception&> maybe_exception, sol::string_view description);
+	enum LuaLibs
+	{
+		Base,
+		Package,
+		Os,
+		Coroutine,
+		Table,
+		String,
+		Math,
+		IO,
+		Utf8
+	};
 
-public:
-	LuaVm();
-	~LuaVm();
 
-	sol::state* getLua();
+	class LuaVm
+	{
+	private:
+		sol::state lua;
+		static int runtime_exception_handler(lua_State* L, sol::optional<const std::exception&> maybe_exception, sol::string_view description);
 
-	void loadFileAndRun(std::string file);
-	sol::protected_function_result runFunction(std::string name);
-};
+	public:
+		LuaVm(LuaLibs... libs);
+		~LuaVm();
+
+		void loadFileAndRun(std::string file);
+		sol::state* getLua();
+		sol::protected_function_result runFunction(std::string name);
+	};
+}
--- LuaInCpp/main.cpp
+++ LuaInCpp/main.cpp
@@ -1,10 +1,24 @@
 #include "Lua/Lua.h"
 
-
-int main() 
+int main(int argc, char** argv)
 {
-	LuaVm lua;
-	lua.loadFileAndRun("script.lua");
+	std::string path;
+	if (!argv[1]) 
+	{
+		spdlog::warn("No file specified defualting to script.lua");
+		path = "script.lua";
+	}
+	else {
+		path = argv[1];
+	}
+	if (path == "")
+	{
+		spdlog::warn("No file specified defualting to script.lua");
+		path = "script.lua";
+	}
 
-	lua.runFunction("update");
+
+	spdlog::debug(path);
+	Lua::LuaVm lua;
+	lua.loadFileAndRun(path);
 }
